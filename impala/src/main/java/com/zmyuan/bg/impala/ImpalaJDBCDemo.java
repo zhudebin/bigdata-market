18357d78ee487e075f9cebf982db61662b51f28c
zhudebin <zdb_hn@163.com>
2016-05-10 09:22:39 +0800

    impala insert

--- impala/src/main/java/com/zmyuan/bg/impala/ImpalaJDBCDemo.java
+++ impala/src/main/java/com/zmyuan/bg/impala/ImpalaJDBCDemo.java
@@ -14,10 +14,42 @@ public class ImpalaJDBCDemo {
     private static final String
             CONNECTION_URL =
             "jdbc:impala://" + IMPALAD_HOST + ':' + IMPALAD_JDBC_PORT + "/default;auth=noSasl";
+    private static final String
+            CONNECTION_FILE_URL =
+            "jdbc:impala://" + IMPALAD_HOST + ':' + IMPALAD_JDBC_PORT + "/default;auth=noSasl";
 
     private static final String JDBC_DRIVER_NAME = "com.cloudera.impala.jdbc41.Driver";
 
     public static void main(String[] args) {
+        testQuery();
+
+
+    }
+
+    public static void testInsert() {
+        // insert into t1 partition(x=10, y='a')
+
+        Connection  con = null;
+        try {
+            con = getFileConnection();
+            con.createStatement().executeUpdate("insert into t1 partition(x=10, y='a') VALUE ()");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void testQuery() {
         System.out.println("\n=============================================");
         System.out.println("Cloudera Impala JDBC Example");
         System.out.println("Using Connection URL: " + CONNECTION_URL);
@@ -63,5 +95,9 @@ public class ImpalaJDBCDemo {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName(JDBC_DRIVER_NAME);
         return DriverManager.getConnection(CONNECTION_URL);
+    }
+    public static Connection getFileConnection() throws SQLException, ClassNotFoundException {
+        Class.forName(JDBC_DRIVER_NAME);
+        return DriverManager.getConnection(CONNECTION_FILE_URL);
     }
 }
